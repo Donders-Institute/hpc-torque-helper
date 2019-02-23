4c1439a8e0fc504bb4ffb2c22ab13619db4e71b0
Hurng-Chun Lee <hclee.work@gmail.com>
2019-02-23 12:35:03 +0100

    Add implementation of TLS server

--- api/gencode.sh
+++ api/gencode.sh
@@ -4,5 +4,9 @@
 # - protobuf: https://github.com/google/protobuf/releases
 # - protoc-gen-go: go get -u github.com/golang/protobuf/protoc-gen-go
 
+# use the following commands to fix the issue: https://github.com/golang/protobuf/issues/763
+# $ git -C $GOPATH/src/github.com/golang/protobuf checkout v1.2.0
+# $ go install github.com/golang/protobuf/protoc-gen-go
+
 # The following command generate client and server gRPC interfaces in ../internal/grpc.pb.go
 protoc --go_out=plugins=grpc:../internal grpc.proto
